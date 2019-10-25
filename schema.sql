b6abd50c2952f9a126a41aba42db0e279bac2921
huangrkul <huangrkul@gmail.com>
2019-10-25 09:19:57 -0700

    update with yesterday's lab

---
+++ schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE geocode;
+CREATE TABLE IF NOT EXISTS geocode(
+  id SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  formatted_query VARCHAR(255),
+  latitude FLOAT,
+  longitude FLOAT
+);
+
+
+SELECT * FROM geocode;
