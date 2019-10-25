256b4e420e931f7e025aa0239097e85849ac682e
huangrkul <huangrkul@gmail.com>
2019-10-25 10:18:14 -0700

    latest updates will driver

--- server.js
+++ server.js
@@ -21,10 +21,6 @@ app.get('/yelp', handleYelp);
 app.get('*', handleError);
 
 
-//cached data:
-let storedUrls = {};
-
-
 function handleLocation(request, response) {
   const location = request.query.data;
 
@@ -110,12 +106,9 @@ function Weather(day) {
 
 function handleTrails(request, response) {
   const locationObj = request.query.data;
-  //console.log(`trail: ${trail}`);
+
   const url = `https://www.hikingproject.com/data/get-trails?lat=${locationObj.latitude}&lon=${locationObj.longitude}&key=${process.env.TRAILS_API_KEY}`;
 
-  // console.log('using cached url', storedUrls[url]);
-  response.send(storedUrls[url]);
-  console.log('making the api call to trails');
   superagent.get(url)
     .then(resultsFromSuperagent => {
       let trailsArr = resultsFromSuperagent.body.trails.map(prop => {
@@ -144,34 +137,29 @@ function Trail(obj) {
   this.condition_time = obj.conditionDate.split(' ')[1];
 }
 
-function handleYelp() {
+function handleYelp(request, response) {
   const locationObj = request.query.data;
   const url = `https://api.yelp.com/v3/businesses/search?location=${locationObj.search_query}`;
   superagent.get(url)
-  .set('Authorization', Bearer ${process.env.YELP_API_KEY})
-  .then(resultsFromSuperagent => {
-    let yelpArr = resultsFromSuperagent.body.businesses[0].map(prop => {
-      return new Yelp(prop);
+    .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
+    .then(resultsFromSuperagent => {
+      let yelpArr = resultsFromSuperagent.body.businesses.map(prop => {
+        return new Yelp(prop);
+      })
+      response.status(200).send(yelpArr);
     })
-    response.status(200).send(yelpArr);
-  })
-  .catch(error => {
-    console.error(error);
-    response.send(error).status(500);
-  });
+    .catch(error => {
+      console.error(error);
+      response.send(error).status(500);
+    });
 }
 
 function Yelp(obj) {
-    // "name": "Pike Place Chowder",
-    // "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/ijju-wYoRAxWjHPTCxyQGQ/o.jpg",
-    // "price": "$$   ",
-    // "rating": "4.5",
-    // "url": "https://www.yelp.com/biz/pike-place-chowder-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA"
-    this.name = obj.businesses[0].name;
-    this.image_url = obj.businesses[0].image_url;
-    this.price = obj.businesses[0].price;
-    this.rating = obj.businesses[0].rating;
-    this.url = obj.businesses[0].url;
+  this.name = obj.name;
+  this.image_url = obj.image_url;
+  this.price = obj.price;
+  this.rating = obj.rating;
+  this.url = obj.url;
 }
 
 function handleError(request, response) {
